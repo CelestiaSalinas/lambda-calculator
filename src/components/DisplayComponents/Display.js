b2141aa76d96752a355d29dc030a5217718ffd24
Celestia Salinas <csalinas3671@gmail.com>
2019-11-05 17:18:21 -0500

    complete

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { userState } from "react";
 
-const Display = () => {
-  return <div className="display">{/* Display any props data here */}</div>;
+const Display = props => {
+  return <div className="display">{/* Display any props data here */}0</div>;
 };
+
+export default Display;
